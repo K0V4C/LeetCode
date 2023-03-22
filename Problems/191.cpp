2f8f36e9572a8a7ca75f0e6e51df8e24487e4c01
Lazar Kovacevic <lazarkovacevic2002@google.com>
2023-03-22 11:03:34 +0100

    Binary 1s

---
+++ Problems/191.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        
+        unsigned int cnt = 0;
+
+        while(n){
+
+            cnt += n & 1;
+            n >>= 1;
+        }
+
+        return cnt;
+
+    }
+};
