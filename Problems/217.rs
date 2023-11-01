1c8068bf0f9226dc938cc8b31fcb4581cb4f18e9
Lazar Kovacevic <lazarkovacevic2002@gmail.com>
2023-11-01 14:43:38 +0100

    fix

--- Problems/217.rs
+++ Problems/217.rs
@@ -1,12 +1,23 @@
-use std::collections::HashSet;
 impl Solution {
-    
-    pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+    pub fn is_anagram(s: String, t: String) -> bool {
+        let mut map: Vec<usize> = vec![0; 26];
 
-        let s = "anagram";
-        let b = "anagram";
+        if s.len() != t.len() {
+            return false;
+        }
 
-        
+        for i in s.as_bytes() {
+            map[(i - b'a') as usize] += 1;
+        }
 
+        for i in t.as_bytes() {
+            if map[(i - b'a') as usize] == 0 {
+                return false;
+            }
+
+            map[(i - b'a') as usize] -= 1;
+        }
+
+        true
     }
 }
