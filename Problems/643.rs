74c15232a7fd8e1ea8f340092597e88622261c6b
Lazar Kovacevic <lazarkovacevic2002@gmail.com>
2023-11-05 14:30:47 +0100

    643

--- Problems/643.rs
+++ Problems/643.rs
@@ -15,3 +15,16 @@ impl Solution {
         maximum
     }
 }
+
+// cooler one
+
+impl Solution {
+    pub fn find_max_average(nums: Vec<i32>, k: i32) -> f64 {
+        nums.windows(k as usize)
+            .map(|x| x.iter().sum::<i32>())
+            .max()
+            .unwrap() as f64
+            / k as f64
+    }
+}
+
